4fadf8aa1035f1878fe934a5e1669775edd04193
nemu2021 <slowslow@nemusoft.co.kr>
2023-11-04 19:46:14 +0900

    함수 옮기기 - amountFor 함수에 class 안에  만든 함수 위임

--- ch01/createStatementData.js
+++ ch01/createStatementData.js
@@ -9,10 +9,9 @@ export default function createStatementData(invoice, plays){
   
 
   function enrichPerformance(aPerformance){
-    // 공연료 계산기 생성
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
-    result.play = calculator.play;
+    result.play = calculator.play;  
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
@@ -22,28 +21,11 @@ export default function createStatementData(invoice, plays){
     return  plays[aPerformance.playID];
   }
 
+  // 함수 위임하기
   function amountFor(aPerformance){
-    let result = 0;
-
-    switch(aPerformance.play.type){
-      case "tragedy":
-        result = 40000;
-        if(aPerformance.audience > 30){
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if(aPerformance.audience > 20){
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알수 없는 쟝르: ${aPerformance.play.type}`); 
-    }
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   }
+
   function volumeCreditsFor(aPerformance){
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
@@ -72,5 +54,29 @@ class PerformanceCalculator{
   constructor(aPerformance, aPlay){
     this.performance = aPerformance;
     this.play = aPlay;
+  }
+
+  // 함수 옮기기
+  get amount(){
+    let result = 0;
+
+    switch(this.play.type){
+      case "tragedy":
+        result = 40000;
+        if(this.performance.audience > 30){
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if(this.performance.audience > 20){
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`알수 없는 쟝르: ${this.play.type}`); 
+    }
+    return result;
   }
 }
