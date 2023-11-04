78c4240d113dcf01cc132e71c5ba55087fa8120b
nemu2021 <slowslow@nemusoft.co.kr>
2023-11-04 14:42:20 +0900

    파일 쪼개기

---
+++ ch01/createStatementData.js
@@ -0,0 +1,68 @@
+export default function createStatementData(invoice, plays){
+  // 중간 데이터 구조를 인수로 전달
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+  return statementData;
+  
+
+  function enrichPerformance(aPerformance){
+    // 불변 데이터를 위해 얕은 복사 수행
+    const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
+    result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
+    return result;
+  }
+
+  function playFor(aPerformance){
+    return  plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance){
+    let result = 0;
+
+    switch(aPerformance.play.type){
+      case "tragedy":
+        result = 40000;
+        if(aPerformance.audience > 30){
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if(aPerformance.audience > 20){
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알수 없는 쟝르: ${aPerformance.play.type}`); 
+    }
+    return result;
+  }
+  function volumeCreditsFor(aPerformance){
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+
+    if("comedy" === aPerformance.play.type) {
+      result += Math.floor(aPerformance.audience / 5);
+    }
+    return result;
+  }
+
+  function totalVolumeCredits(data){
+    return data.performances
+      .reduce((total, p) => total + p.volumeCredits, 0);
+    
+  }
+
+  function totalAmount(data){
+    return data.performances
+      .reduce((total, p) => total + p.amount, 0);
+
+  }
+}
+
