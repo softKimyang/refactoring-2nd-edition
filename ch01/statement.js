444152f77a9e76644ed2b5d6942c1b3f18a512c0
nemu2021 <slowslow@nemusoft.co.kr>
2023-11-04 14:17:43 +0900

    공연정보를 중간 데이터로 옮김

--- ch01/statement.js
+++ ch01/statement.js
@@ -2,9 +2,20 @@ export default function statement(invoice, plays){
   // 중간 데이터 구조를 인수로 전달
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance);
 
   return renderPlainText(statementData,  plays);
+
+  function enrichPerformance(aPerformance){
+    // 불변 데이터를 위해 얕은 복사 수행
+    const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
+    return result;
+  }
+
+  function playFor(aPerformance){
+    return  plays[aPerformance.playID];
+  }
 }
 
 // 공연정보를 중간 데이터로 옮김
@@ -12,7 +23,7 @@ function renderPlainText(data,  plays){
   let result = `청구 내역 (고객명: ${data.customer})\n`;
 
   for(let perf of data.performances){
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))}(${perf.audience})석\n`;
+    result += `${perf.play.name}: ${usd(amountFor(perf))}(${perf.audience})석\n`;
   }
 
   result += `총액: ${usd(totalAmount())}\n`;
@@ -22,7 +33,7 @@ function renderPlainText(data,  plays){
   function amountFor(aPerformance){
     let result = 0;
 
-    switch(playFor(aPerformance).type){
+    switch(aPerformance.play.type){
       case "tragedy":
         result = 40000;
         if(aPerformance.audience > 30){
@@ -37,20 +48,18 @@ function renderPlainText(data,  plays){
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알수 없는 쟝르: ${playFor(aPerformance).type}`); 
+        throw new Error(`알수 없는 쟝르: ${aPerformance.play.type}`); 
     }
     return result;
   }
 
-  function playFor(aPerformance){
-    return  plays[aPerformance.playID];
-  }
+
 
   function volumeCreditsFor(aPerformance){
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
 
-    if("comedy" === playFor(aPerformance).type) {
+    if("comedy" === aPerformance.play.type) {
       result += Math.floor(aPerformance.audience / 5);
     }
     return result;
