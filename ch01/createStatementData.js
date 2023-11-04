a98931c7dd3bceb5dab220896653038e42032a6d
nemu2021 <slowslow@nemusoft.co.kr>
2023-11-04 20:14:14 +0900

    다형성 활용 - factory class 생성 후 타입별 class 에서 상속

--- ch01/createStatementData.js
+++ ch01/createStatementData.js
@@ -9,7 +9,7 @@ export default function createStatementData(invoice, plays){
   
 
   function enrichPerformance(aPerformance){
-    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+    const calculator = new createPerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;  
     result.amount = calculator.amount;
@@ -20,16 +20,6 @@ export default function createStatementData(invoice, plays){
   function playFor(aPerformance){
     return  plays[aPerformance.playID];
   }
-
-  //amountFor 삭제
-  // function amountFor(aPerformance){
-  //   return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-  // }
-
-  // volumeCreditsFor 삭제
-  // function volumeCreditsFor(aPerformance){
-  //   return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
-  // }
 
   function totalVolumeCredits(data){
     return data.performances
@@ -51,37 +41,45 @@ class PerformanceCalculator{
     this.play = aPlay;
   }
 
-  // 함수 옮기기
   get amount(){
-    let result = 0;
-
-    switch(this.play.type){
-      case "tragedy":
-        result = 40000;
-        if(this.performance.audience > 30){
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if(this.performance.audience > 20){
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`알수 없는 쟝르: ${this.play.type}`); 
-    }
-    return result;
+    throw new Error(`서브 클래스에서 처리`); 
   }
 
   get volumeCredits(){
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
+    return  Math.max(this.performance.audience - 30, 0);
+  }
+}
 
-    if("comedy" === this.play.type) {
-      result += Math.floor(this.performance.audience / 5);
+// factory class 생성
+function createPerformanceCalculator(aPerformance, aPlay){
+  switch(aPlay.type){
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+    default: 
+     throw new Error(`알 수 없는 쟝르: ${aPlay.type}`);
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator{
+  get amount(){
+    let result = 40000;
+    if(this.performance.audience > 30){
+      result += 1000 * (this.performance.audience - 30);
     }
     return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator{
+  get amount(){
+    let result = 30000;
+    if(this.performance.audience > 20){
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+  get volumeCredits(){
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
