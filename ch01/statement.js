6ecb739c4c9f95c9dd0233798a568f7f6a9750ce
nemu2021 <slowslow@nemusoft.co.kr>
2023-11-04 12:20:11 +0900

    매개변수 이름 변경. return 변수는 result로

--- ch01/statement.js
+++ ch01/statement.js
@@ -32,27 +32,27 @@ export default function statement(invoice, plays){
   return result;
 
 
-  // 함수 추출하기. 
-  function amountFor(perf, play){
-    let thisAmount = 0;
-    console.log(`perf : ${play.type}`)
+  // 매개변수 이름 변경. return 변수는 result로
+  function amountFor(aPerformance, play){
+    let result = 0;
+    console.log(`aPerformance : ${play.type}`)
     switch(play.type){
       case "tragedy":
-        thisAmount = 40000;
-        if(perf.audience > 30){
-          thisAmount += 1000 * (perf.audience - 30);
+        result = 40000;
+        if(aPerformance.audience > 30){
+          result += 1000 * (aPerformance.audience - 30);
         }
         break;
       case "comedy":
-        thisAmount = 30000;
-        if(perf.audience > 20){
-          thisAmount += 10000 + 500 * (perf.audience - 20);
+        result = 30000;
+        if(aPerformance.audience > 20){
+          result += 10000 + 500 * (aPerformance.audience - 20);
         }
-        thisAmount += 300 * perf.audience;
+        result += 300 * aPerformance.audience;
         break;
       default:
         throw new Error(`알수 없는 쟝르: ${play.type}`);
     }
-    return thisAmount;
+    return result;
   }
 }
