d016ecbed5dfc7b87f3f9acce6e1e3a8af5b7526
nemu2021 <slowslow@nemusoft.co.kr>
2023-11-04 19:59:59 +0900

    함수 옮기기 - calculator.volumeCredits

--- ch01/createStatementData.js
+++ ch01/createStatementData.js
@@ -13,7 +13,7 @@ export default function createStatementData(invoice, plays){
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;  
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
@@ -26,15 +26,10 @@ export default function createStatementData(invoice, plays){
   //   return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   // }
 
-  function volumeCreditsFor(aPerformance){
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-
-    if("comedy" === aPerformance.play.type) {
-      result += Math.floor(aPerformance.audience / 5);
-    }
-    return result;
-  }
+  // volumeCreditsFor 삭제
+  // function volumeCreditsFor(aPerformance){
+  //   return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
+  // }
 
   function totalVolumeCredits(data){
     return data.performances
@@ -76,6 +71,16 @@ class PerformanceCalculator{
         break;
       default:
         throw new Error(`알수 없는 쟝르: ${this.play.type}`); 
+    }
+    return result;
+  }
+
+  get volumeCredits(){
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+
+    if("comedy" === this.play.type) {
+      result += Math.floor(this.performance.audience / 5);
     }
     return result;
   }
